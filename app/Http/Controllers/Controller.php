0d06cfa4c4a929c4ba1f580dee46eb44db9d17ba
onwuasoanya_george <ndu4george@gmail.com>
2019-07-21 11:44:22 +0100

    added swagger initialization  annotations and json file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,25 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+
+/**
+ * @SWG\Swagger(
+ *     schemes={"http","https"},
+ *     consumes={"application/x-www-form-urlencoded"},
+ *     @SWG\Info(
+ *         version="1.0",
+ *         title="Gage API",
+ *         description="This is the API service for Gage app",
+ *         @SWG\Contact(
+ *             url="#"
+ *         ),
+ *     ),
+ *     @SWG\ExternalDocumentation(
+ *         description="Find out more about Gage",
+ *         url="#"
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
